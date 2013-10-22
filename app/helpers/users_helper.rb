7b7527c867dca7f8b8b6f8b8f754a90dc3b5a097
Izzy Park <isy012@hotmail.com>
2013-10-21 23:20:59 -0400

    user page refresh

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,8 @@ module UsersHelper
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}"
 		image_tag(gravatar_url, :alt => user.name,
-								:class => "gravatar img-circle")
+								:class => "gravatar img-circle",
+								:size => "50")
 	end
 
 end
