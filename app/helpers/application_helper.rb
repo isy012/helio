f54d9671de4b1b9759427ba02d3c3e0da3391bff
Izzy Park <isy012@hotmail.com>
2013-12-30 06:16:28 -0800

    fixed user login variable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	#Return full title on a per-page basis.
 	def full_title(page_title)
-		base_title= "Helio"
+		base_title= "Online Book Reviews"
 		if page_title.empty?
 			base_title
 		else
